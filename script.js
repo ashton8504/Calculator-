a5310237cd6c772387e6747cf177c563412ef450
Ashton <92445005+ashton8504@users.noreply.github.com>
2022-02-17 12:28:04 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//Function Add
+function add(num1, num2){
+  return num1 + num2;
+};
+
+//Function Subtract
+function subtract(num1, num2){
+  return num1 - num2;
+};
+
+//Function Multiply
+function multiply(num1, num2){
+  return num1 * num2;
+};
+
+//Function Divide
+function divide(num1, num2){
+  return num1 / num2;
+};
